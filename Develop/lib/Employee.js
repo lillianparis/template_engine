d0319e32d552be6adaa1f813d581a4adfb8e89bb
Lillian Paris <lillian.paris529@gmail.com>
2020-10-17 09:00:10 -0700

    Added test.js files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Employee class
+// refer to activity 20 = shape.js
+// parent is employee
+//   * name
+//   * id
+//   * email
+//   * getName()
+//   * getId()
+//   * getEmail()
+//   * getRole() // Returns 'Employee'
+
+class employee {
+    constructor()
+}
+
+
+module.exports = employee;
