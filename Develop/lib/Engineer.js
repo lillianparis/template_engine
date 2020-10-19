d92df35ded1529e6e8979f0bdfe2537a1de83c6c
Lillian Paris <lillian.paris529@gmail.com>
2020-10-18 20:03:48 -0700

    Created class for Engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// * github  // GitHub username
+
+// * getGithub()
+
+// * getRole() // Overridden to return 'Engineer'
+
+const Employee = require("./Employee")
+
+class Engineer extends Employee {
+    constructor(name, email, id, github) {
+        super(name, email, id);
+        this.github = github;
+    }
+}
+github.printInfo()
+module.ecports = Engineer
